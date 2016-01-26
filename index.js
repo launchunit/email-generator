f2a06bf4ef8ac716eb91a9cd9bc865d4d5cde79b
Karan Sakhuja <kevin.sakhuja@gmail.com>
2016-01-26 14:03:06 -0500

    Cleanup and fixes

--- index.js
+++ index.js
@@ -6,43 +6,17 @@
  * @private
  */
 const _ = require('lodash'),
-  nameParser = require('./lib/name_parser');
+  nameParser = require('./lib/name_parser'),
+  RULES = require('./lib/rules');
 
 
-const RULES = [
-  '${name.firstName}',
-  '${name.lastName}',
-
-  '${name.firstName}${name.lastName}',
-  '${name.firstName}.${name.lastName}',
-  '${name.firstName}_${name.lastName}',
-
-  '${name.lastName}${name.firstName}',
-  '${name.lastName}.${name.firstName}',
-  '${name.lastName}_${name.firstName}',
-
-  '${name.firstNameInitial}${name.lastName}',
-  '${name.firstNameInitial}.${name.lastName}',
-  '${name.firstNameInitial}_${name.lastName}',
-
-  '${name.firstName}${name.lastNameInitial}',
-  '${name.firstName}.${name.lastNameInitial}',
-  '${name.firstName}_${name.lastNameInitial}',
-
-  '${name.lastNameInitial}${name.firstName}',
-  '${name.lastNameInitial}.${name.firstName}',
-  '${name.lastNameInitial}_${name.firstName}',
-
-  '${name.lastName}${name.firstNameInitial}',
-  '${name.lastName}0${name.firstNameInitial}',
-  '${name.lastName}_${name.firstNameInitial}',
-
-  '${name.firstNameInitial}.${name.lastNameInitial}',
-  '${name.firstNameInitial}_${name.lastNameInitial}',
-  '${name.firstName}.${name.middleNameInitial}.${name.lastName}',
-];
-
-function ruleFn(ruleStr, parsedName) {
+/**
+ * @params ruleStr {String}
+ * @params name {Object}
+ *
+ * @public
+ */
+exports.buildRuleFn = (ruleStr, parsedName) => {
   const fn = new Function('name','return `'+ruleStr+'`');
   return new Function('domain', 'return `' +
                         fn(parsedName).toLowerCase() +
@@ -63,13 +37,17 @@ exports.build = name => {
   parsedName.firstName = _.capitalize(parsedName.firstName).trim();
   parsedName.lastName = _.capitalize(parsedName.lastName).trim();
 
+  parsedName.middleName = parsedName.middleName || '';
+  parsedName.middleName = _.capitalize(parsedName.middleName).trim();
+
   parsedName.firstNameInitial = parsedName.firstName.substring(0,1);
   parsedName.lastNameInitial = parsedName.lastName.substring(0,1);
+  parsedName.middleNameInitial = parsedName.middleName.substring(0,1);
 
 
   const emails = _.map(RULES, ruleStr => {
     return {
-      ruleFn: ruleFn(ruleStr, parsedName),
+      ruleFn: exports.buildRuleFn(ruleStr, parsedName),
       ruleStr: ruleStr
     }
   });
@@ -79,12 +57,3 @@ exports.build = name => {
     emails: emails
   };
 };
-
-
-/**
- * @params ruleStr {String}
- * @params name {Object}
- *
- * @public
- */
-exports.ruleFn = ruleFn;
